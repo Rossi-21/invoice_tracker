d10ce4050fe5658d2063c737efc6d90e0fd4265d
Rossi21 <rossi21@gmail.com>
2024-01-29 07:13:22 -0800

    Input validation and css

--- invoice_app/admin.py
+++ invoice_app/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
-
-from django.contrib import admin
 from .models import *
 
-admin.site.register(Invoice)
+
+class InvoiceAdmin(admin.ModelAdmin):
+    list_display = ('vendor', 'total', 'department', 'date', 'created_at')
+    list_filter = ('vendor', 'department', 'date')
+    search_fields = ['vendor__name']
+
+
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(Vendor)
 admin.site.register(Department)
